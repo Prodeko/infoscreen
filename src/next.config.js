199dc9d4db58cf163026eec2f146b8b0da8b53be
rriski <timo.h.riski@outlook.com>
2019-07-13 18:28:30 +0300

    Upgrade dependencies and setup for nextjs 9

--- src/next.config.js
+++ src/next.config.js
@@ -1,7 +1,6 @@
-const withTypescript = require("@zeit/next-typescript");
 const withLess = require("@zeit/next-less");
 const withPlugins = require("next-compose-plugins");
 
-module.exports = withPlugins([withTypescript, withLess], {
+module.exports = withPlugins([withLess], {
   distDir: "../.next"
 });
