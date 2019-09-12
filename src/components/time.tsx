0148ffa62d9d3698b460dace76f72d3a6a3c664f
rriski <timo.h.riski@outlook.com>
2019-09-12 14:14:01 +0300

    Add date

--- src/components/time.tsx
+++ src/components/time.tsx
@@ -4,19 +4,33 @@ import { getTime } from '../hooks'
 
 const TimeContainer = styled.span`
   position: absolute;
+  top: 5px;
   left: 50px;
   color: white;
-  font-size: 70px;
+  font-size: 3.2rem;
   font-weight: 900;
   font-family: 'Arial', Courier, monospace;
   letter-spacing: -2px;
   text-shadow: ${({ theme }) => theme.textShadow};
 `
 
+const StyledDate = styled.p`
+  margin-left: 7px;
+  font-size: 1.5rem;
+  letter-spacing: 2px;
+`
+
 const Time: React.FC<{}> = (): JSX.Element => {
   const time = getTime()
+  const d: Date = new Date()
+  const date = d.toLocaleDateString('fi-FI')
 
-  return <TimeContainer>{time}</TimeContainer>
+  return (
+    <TimeContainer>
+      {time}
+      <StyledDate>{date}</StyledDate>
+    </TimeContainer>
+  )
 }
 
 export default Time
