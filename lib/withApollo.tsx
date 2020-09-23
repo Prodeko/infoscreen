9f4d2b50f9ddbcbfc1ae590e36a21430b41fc86b
Timo Riski <timo.h.riski@aalto.fi>
2020-09-23 18:13:15 +0300

    Update apollo imports

--- lib/withApollo.tsx
+++ lib/withApollo.tsx
@@ -1,26 +1,13 @@
 import withApollo from 'next-with-apollo'
-import {
-  InMemoryCache,
-  IntrospectionFragmentMatcher,
-} from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
-import ApolloClient from 'apollo-client'
+import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const GRAPHQL_URL =
   'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
 
-const fragmentMatcher = new IntrospectionFragmentMatcher({
-  introspectionQueryResultData: {
-    __schema: {
-      types: [],
-    },
-  },
-})
-
 export default withApollo(
   ({ initialState }) =>
     new ApolloClient({
-      link: new HttpLink({ uri: GRAPHQL_URL }),
-      cache: new InMemoryCache({ fragmentMatcher }).restore(initialState || {}),
+      uri: GRAPHQL_URL,
+      cache: new InMemoryCache().restore(initialState || {}),
     }),
 )
