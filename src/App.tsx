681d9f42bc16a8bcf243cc5a7ec57ead30bdde84
Leevi Jäkälä <jakala.leevi@gmail.com>
2024-04-14 11:24:14 +0300

    Nice clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
+import { useEffect, useState } from "react"
+
+/**
+ * Custom hook to get the current time from the browser.
+ * Updates once every second.
+ * @returns The current time as a Date object.
+ */
+const useClock = () => {
+	const [time, setTime] = useState<Date>(new Date())
+
+	useEffect(() => {
+		const interval = setInterval(() => setTime(new Date()), 1000)
+		return () => clearInterval(interval)
+	}, [])
+
+	return time
+}
+
 const App = () => {
+	const time = useClock()
+	const timeHoursMinutes = time.toLocaleTimeString("fi-FI", {
+		"minute": "2-digit",
+		"hour": "2-digit",
+	}).replace(/\./g, ":")
+
 	return (
 		<div className="grid min-h-screen grid-cols-[2fr_2fr_1fr] grid-rows-[100px_auto_100px] gap-10 bg-gray-50 p-4">
 			<h1 className="text-xl font-bold underline bg-blue-100 col-span-2">
 				Prodeko
 			</h1>
-			<h1 className="text-xl font-bold underline bg-green-200">Time</h1>
+			<h1 className="text-8xl flex flex-col font-semibold bg-green-200 text-center items-center justify-center">{
+				timeHoursMinutes
+			}</h1>
 			<h1 className="text-xl font-bold underline bg-orange-100">Kanttiinit</h1>
 			<h1 className="text-xl font-bold underline bg-orange-100 row-span-2">
 				Ilmos
