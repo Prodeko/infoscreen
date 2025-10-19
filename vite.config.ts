57dd324e832cd75552d907bc970ba073bac3c997
aksun1 <aksunpostit@gmail.com>
2025-10-19 14:22:24 +0300

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-	// Throw an error if the password is not set in production
+export default defineConfig(({ command, mode }) => {
+	// Throw an error if the password is not set during build
 	if (
-		mode === 'production' &&
+		command === 'build' &&
 		!loadEnv(mode, process.cwd(), '').VITE_KILTISKAMERA_ON_AIR_PASSWORD
 	) {
 		throw new Error(
